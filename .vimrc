7af5061abc86ba887cdb4af6bd413470a0c7db31
adam.oakes <oakeada@linux-pc-85178.echostar.com>
2020-07-27 10:40:03 -0600

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,4 @@
+set number
+autocmd BufNewFile,BufRead *.aliases set syntax=sh
+autocmd BufNewFile,BufRead *.suse_aliases set syntax=sh
+autocmd BufNewFile,BufRead *.vimrc set syntax=sh
---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir ~/.conf
+cp .aliases ~/.conf
+cp .suse_aliases ~/.conf
+cp .vimrc ~/.conf
+cp bookmarks ~/.conf
+
+echo "source ~/.conf/.aliases" >> ~/.bashrc
+sudo echo "source ~/.conf/.vimrc" >> ~/etc/vimrc
