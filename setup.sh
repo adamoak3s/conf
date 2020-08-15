41e8ae04e74d092cfa037433f35b44e34659d643
adamoak3s <adamoak3s@gmail.com>
2020-08-15 11:57:52 -0600

    fixes idiocy

--- setup.sh
+++ setup.sh
@@ -1,8 +1,2 @@
-mkdir ~/.conf
-cp .aliases ~/.conf
-cp .suse_aliases ~/.conf
-cp .vimrc ~/.conf
-cp bookmarks ~/.conf
-
 echo "source ~/.conf/.aliases" >> ~/.bashrc
 sudo echo "source ~/.conf/.vimrc" >> /etc/vimrc
